58b3a7aa2b7020eeee5c89cc93fce37cbcad24d2
SangwanYu <ryusangwan12@gmail.com>
2025-09-23 18:03:21 +0900

    refactor: 상품그리드 개수 변경

--- src/components/search/ProductGrid.tsx
+++ src/components/search/ProductGrid.tsx
@@ -133,7 +133,7 @@ const ProductGrid = () => {
       </div>
 
       {/* 제품 그리드 */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
         {products.map((product) => {
           // 표시할 이미지 결정 (제품 사진 우선, 없으면 기본 이미지)
           const displayImage = product.productImage || "/gyul.png";
